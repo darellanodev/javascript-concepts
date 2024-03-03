ffcee815f4ab9549351ba0283148eb8d8022f511
darellanodev <darellano.aliaga@gmail.com>
2024-03-03 18:41:37 +0100

    first commit

---
+++ pojo.js
@@ -0,0 +1,10 @@
+const levelOne = {
+    id: 1,
+    rowMapTiles: ['000001','111111','100000','100000','111111','000001'],
+    name: 'serpent',
+    favorite: true,
+}
+
+console.log(levelOne.id); // 1
+console.log(levelOne.name); // 'serpent'
+console.log(levelOne.stars); // undefined
